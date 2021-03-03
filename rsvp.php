da85f1ceedd9ae9aeedcda5d43ace0f6a9f28fcd
augmentchinedu <augmentchinedu@gmail.com>
2021-03-03 10:10:02 +0100

    Lagos

---
+++ rsvp.php
@@ -0,0 +1,8 @@
+<?php
+$to = "augmentchinedu@gmail.com";
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: rsvp@kelvinandprincess2021.com";
+
+mail($to,$subject,$txt,$headers);
+?>
